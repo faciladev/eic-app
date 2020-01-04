4e4ca818e9cc3f7433881f8e8dcb4e18fa52e8bf
Fasil Hailu <email.fasilhailu@gmail.com>
2020-01-04 21:53:59 +0300

    Sector lists display added.

--- lib/screens/sector.dart
+++ lib/screens/sector.dart
@@ -89,24 +89,45 @@ class _SectorScreenState extends State<SectorScreen> {
     while (nodeStack.length > 0) {
       var node = nodeStack.removeLast();
       try {
+        //check if node is a dictionary
         Map<String, dynamic> mapData = node;
+
         mapData.forEach((String key, dynamic value) {
-          nodeStack.add('__title__$key');
-          nodeStack.add(value);
-          nodeStack.add('__divider__');
+          if (key == 'table') {
+            List rows = value['rows'];
+            List headers = value['headers'];
+            List<DataColumn> dataColumns = _buildDataColumns(headers, rows);
+            List<DataRow> dataRows = _buildDataRows(rows, headers);
+            _addTableToPage(body, dataColumns, dataRows);
+            _buildVerticalSpace(body);
+          } else if (key == 'list') {
+            List<Widget> children = _buildListChildren(value);
+            _addListToPage(body, children);
+            _buildVerticalSpace(body);
+          } else {
+            nodeStack.add('__title__$key');
+            nodeStack.add(value);
+            nodeStack.add('__divider__');
+          }
         });
       } catch (e) {
         try {
+          //Check if node is a List
           List<dynamic> listData = node;
           listData.forEach((item) {
             nodeStack.add(item);
           });
         } catch (e) {
+          //Check if node is a String
           String stringData = node;
 
           if (stringData.startsWith('__title__')) {
             stringData = stringData.replaceFirst('__title__', '');
-            if (stringData == 'text' || stringData == 'images') {
+            if (stringData == 'text' ||
+                stringData == 'images' ||
+                stringData == 'lists' ||
+                stringData == 'tables' ||
+                stringData == 'content') {
               continue;
             }
             body.insert(
@@ -149,5 +170,86 @@ class _SectorScreenState extends State<SectorScreen> {
             children: body,
           )
         : Container();
+  }
+
+  void _addTableToPage(
+      List<Widget> body, List<DataColumn> dataColumns, List<DataRow> dataRows) {
+    body.insert(
+      0,
+      DataTable(
+        columnSpacing: 5.0,
+        columns: dataColumns.length <= 0
+            ? [DataColumn(label: Text(''))]
+            : dataColumns,
+        rows: dataRows.length <= 0
+            ? [
+                DataRow(cells: [DataCell(Text(''))])
+              ]
+            : dataRows,
+      ),
+    );
+  }
+
+  void _addListToPage(List<Widget> body, List<Widget> children) {
+    body.insert(0, Column(children: children));
+  }
+
+  void _buildVerticalSpace(List<Widget> body) {
+    body.insert(
+      0,
+      SizedBox(
+        height: 17.0,
+      ),
+    );
+  }
+
+  List<Widget> _buildListChildren(value) {
+    List<dynamic> lists = value;
+    List<Widget> children = lists.map((list) {
+      return ListTile(
+        leading: Icon(
+          Icons.check_circle,
+          size: 20.0,
+        ),
+        title: Text(
+          list,
+        ),
+      );
+    }).toList();
+    return children;
+  }
+
+  List<DataRow> _buildDataRows(List rows, List headers) {
+    List<DataRow> dataRows = [];
+    if (rows != null &&
+        headers != null &&
+        rows.length > 0 &&
+        headers.length > 0) {
+      for (int num = 0; num < rows.length; num++) {
+        List<DataCell> dataCells = [];
+        if (rows[num].length != headers.length) {
+          //row and column length not equal
+          continue;
+        }
+        rows[num].forEach((cell) {
+          dataCells.add(DataCell(Text(cell)));
+        });
+        dataRows.add(DataRow(cells: dataCells));
+      }
+    }
+    return dataRows;
+  }
+
+  List<DataColumn> _buildDataColumns(List headers, List rows) {
+    List<DataColumn> dataColumns = [];
+    if (headers != null &&
+        rows != null &&
+        headers.length > 0 &&
+        rows.length > 0) {
+      dataColumns = headers.map((header) {
+        return DataColumn(label: Text(header));
+      }).toList();
+    }
+    return dataColumns;
   }
 }
