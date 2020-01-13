51e51f996f160c5de960279086734b3a0fbc5777
Fasil Hailu <email.fasilhailu@gmail.com>
2020-01-13 19:26:46 +0300

    Language page redesign added.

--- lib/screens/language.dart
+++ lib/screens/language.dart
@@ -1,6 +1,5 @@
 import 'package:eicapp/providers/chinese_page.dart';
 import 'package:eicapp/providers/setting.dart';
-// import 'package:eicapp/providers/language.dart';
 import 'package:eicapp/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,85 +17,67 @@ class _LanguageScreenState extends State<LanguageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: Column(
+      body: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background2.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[Colors.transparent, Colors.white],
+          )),
+        ),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0),
-              child: Image.asset(
-                'assets/images/logo.png',
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            SizedBox(
-              height: 17,
-            ),
-            Text(
-              'Select Language | 语',
-              style: TextStyle(
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: Theme.of(context).textTheme.title.fontFamily),
-            ),
-            Divider(
-              height: 30.0,
-            ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 15.0),
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   _buildLanguageOption(Language.English),
-                  Divider(),
+                  Text('|'),
                   _buildLanguageOption(Language.Chinese),
                 ],
               ),
             ),
           ],
         ),
-      ),
+      ]),
     );
   }
 
   _buildLanguageOption(Language language) {
     return Consumer<SettingProvider>(builder: (context, model, _) {
-      String _countryCode, _languageTitle;
+      String _languageTitle;
       if (language == Language.Chinese) {
-        _countryCode = 'cn';
         _languageTitle = '中文';
       } else if (language == Language.English) {
-        _countryCode = 'gb';
-        _languageTitle = 'English';
+        _languageTitle = 'ENG';
       }
       return GestureDetector(
         child: Container(
-          decoration: BoxDecoration(color: Theme.of(context).accentColor),
-          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              _buildCountryImage(country: _countryCode, width: 50.0),
-              Text(
-                _languageTitle,
-                style: Theme.of(context).textTheme.title,
-              ),
-              Icon(Icons.navigate_next),
-            ],
+            child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            _languageTitle,
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 24.0,
+            ),
           ),
-        ),
+        )),
         onTap: () => _languageHandler(context, model, language),
       );
     });
-  }
-
-  Image _buildCountryImage({String country, double width}) {
-    return Image.asset(
-      'icons/flags/png/$country.png',
-      package: 'country_icons',
-      width: width,
-    );
   }
 
   void _languageHandler(context, SettingProvider model, Language language) {
