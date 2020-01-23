ecd93c38779a13affcce19cae0f1af06dc4ba4e8
Fasil Hailu <email.fasilhailu@gmail.com>
2020-01-23 16:11:46 +0300

    Listing style added.

--- lib/widgets/myListing.dart
+++ lib/widgets/myListing.dart
@@ -13,30 +13,35 @@ class MyListing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      splashColor: Theme.of(context).accentColor,
-      onTap: myOnTap != null ? myOnTap : () {},
-      child: Container(
-        margin: EdgeInsets.all(10.0),
-        padding: EdgeInsets.all(10.0),
-        decoration: BoxDecoration(
-            color:
-                shouldGradient ? null : Theme.of(context).secondaryHeaderColor,
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            gradient: shouldGradient
-                ? LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    stops: [0.1, 0.9],
-                    colors: <Color>[
-                      Theme.of(context).primaryColor,
-                      Theme.of(context).secondaryHeaderColor
-                    ],
-                  )
-                : null),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: widgets,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        splashColor: Theme.of(context).accentColor,
+        onTap: myOnTap != null ? myOnTap : () {},
+        child: Container(
+          margin: EdgeInsets.all(10.0),
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              boxShadow: [BoxShadow(blurRadius: 3)],
+              color: shouldGradient
+                  ? null
+                  : Theme.of(context).secondaryHeaderColor,
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              gradient: shouldGradient
+                  ? LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      stops: [0.1, 0.9],
+                      colors: <Color>[
+                        Theme.of(context).primaryColor,
+                        Theme.of(context).secondaryHeaderColor
+                      ],
+                    )
+                  : null),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: widgets,
+          ),
         ),
       ),
     );
